bdd08a2af42017b9827eb546e66d01c870f7507e
TaraRhoseyn <taragulwell@gmail.com>
2021-11-23 19:31:34 +0000

    Add database models for task todo application

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# the 'models.Model' below is making sure our model can do everything the base model from Django can (see import above too)
+class Item(models.Model):
+    # Django will create ID automatically
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    def __str__(self):
+        return self.name
