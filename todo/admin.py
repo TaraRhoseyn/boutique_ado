a3235cb716d528e81a0663952451f82889a6f3de
TaraRhoseyn <taragulwell@gmail.com>
2021-11-27 12:20:03 +0000

    Connect view to template and ate and add template variables to display items from the Item class from the Models Models dir

--- todo/admin.py
+++ todo/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+# Importing our Item class from the models dir
+# This exposes it to admin user
 from .models import Item
 
 # Register your models here.
--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # the 'models.Model' below is making sure our model can do everything the base model from Django can (see import above too)
+
+
 class Item(models.Model):
     # Django will create ID automatically
     name = models.CharField(max_length=50, null=False, blank=False)
