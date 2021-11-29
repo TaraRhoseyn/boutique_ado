718d5fcd2c8cf3db97ba08d4b04f6144b77bb504
TaraRhoseyn <taragulwell@gmail.com>
2021-11-29 20:44:47 +0000

    Add tests for forms

--- todo/test_views.py
+++ todo/test_views.py
@@ -3,9 +3,9 @@ from django.test import TestCase
 # Create your tests here.
 
 
-class TestDjango(TestCase):
-    # 'self' here refers to TestDjango class
+# class TestDjango(TestCase):
+#     # 'self' here refers to TestDjango class
 
 
-    def test_this_thing_work(self):
-        self.assertEqual(1, 0)
+#     def test_this_thing_work(self):
+#         self.assertEqual(1, 0)
